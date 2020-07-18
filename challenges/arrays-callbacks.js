9a6b9d869658a73c51822dfaaa580729ab311209
Ian-Tucker <iantucker2000@gmail.com>
2020-07-17 21:05:25 -0500

    fixing numbers

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -67,7 +67,7 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
-function consume (a, b, cb){
+function consume(a, b, cb){
   return cb(a,b);
 }
 
@@ -77,11 +77,22 @@ function consume (a, b, cb){
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function greeting(first_name, last_name){
+  return "Hello " + first_name + " " + last_name + ", nice to meet you!" 
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
